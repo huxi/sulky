1921c22428c3a8f6654ab9ddb4c33517a4e7c2b3
Joern Huxhorn <huxhorn@users.sourceforge.net>
2009-03-06 23:18:12 +0000

    Moved CodecFileBuffer to own module. Removed codec dependency from buffer module.
    git-svn-id: https://sulky.svn.sourceforge.net/svnroot/sulky/trunk@87 33f67c6c-4047-0410-b7fd-cc5e57a77e41

--- sulky-codec-filebuffer/src/test/java/de/huxhorn/sulky/codec/filebuffer/CodecFileBufferTest.java
+++ sulky-codec-filebuffer/src/test/java/de/huxhorn/sulky/codec/filebuffer/CodecFileBufferTest.java
@@ -15,10 +15,11 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.huxhorn.sulky.buffers;
+package de.huxhorn.sulky.codec.filebuffer;
 
 import de.huxhorn.sulky.codec.Codec;
 import de.huxhorn.sulky.codec.SerializableCodec;
+import de.huxhorn.sulky.buffers.ElementProcessor;
 
 import org.junit.After;
 import static org.junit.Assert.*;
