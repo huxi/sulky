a244b5c1d1028ad517bf6c3ffe4c8e6f5af0030b
Joern Huxhorn <huxhorn@users.sourceforge.net>
2009-12-29 09:41:25 +0100

    Added some tests.

---
+++ sulky-codec/src/test/java/de/huxhorn/sulky/codec/streaming/GZIPStreamingWrapperTest.java
@@ -0,0 +1,42 @@
+/*
+ * sulky-modules - several general-purpose modules.
+ * Copyright (C) 2007-2009 Joern Huxhorn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.huxhorn.sulky.codec.streaming;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class GZIPStreamingWrapperTest
+{
+	@Test
+	public void test() throws IOException
+	{
+		String obj = "Foo";
+		StreamingEncoder<String> encoder = new GZIPStreamingEncoderWrapper<String>(new StreamingSerializableCodec<String>());
+		StreamingDecoder<String> decoder = new GZIPStreamingDecoderWrapper<String>(new StreamingSerializableCodec<String>());
+		ByteArrayOutputStream bos=new ByteArrayOutputStream();
+		encoder.encode(obj, bos);
+		ByteArrayInputStream bis=new ByteArrayInputStream(bos.toByteArray());
+		String decoded = decoder.decode(bis);
+		assertEquals(obj, decoded);
+	}
+}
