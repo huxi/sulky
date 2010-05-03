95573998cafdae09596b5b2fa1d4409c7b1b4cb1
Joern Huxhorn <huxhorn@users.sourceforge.net>
2010-05-03 10:04:23 +0200

    Updated all copyright notices to 2010.

--- sulky-sounds-api/src/main/java/de/huxhorn/sulky/sounds/Sounds.java
+++ sulky-sounds-api/src/main/java/de/huxhorn/sulky/sounds/Sounds.java
@@ -1,6 +1,6 @@
 /*
  * sulky-modules - several general-purpose modules.
- * Copyright (C) 2007-2009 Joern Huxhorn
+ * Copyright (C) 2007-2010 Joern Huxhorn
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
