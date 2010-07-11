965237b6c27c11ef43436c0a2b69ae50f327fb1a
Joern Huxhorn <huxhorn@users.sourceforge.net>
2010-07-11 15:55:10 +0200

    Second try to suppress magic number warnings.

--- sulky-formatting/src/main/java/de/huxhorn/sulky/formatting/SimpleXml.java
+++ sulky-formatting/src/main/java/de/huxhorn/sulky/formatting/SimpleXml.java
@@ -95,7 +95,7 @@ public final class SimpleXml
 	 * @param replacementChar the character to replace invalid characters with.
 	 * @return The in String, with non-valid characters replaced by replacementChar.
 	 */
-	@SuppressWarnings("PMD.MagicNumberCheck")
+	@SuppressWarnings({"MagicNumber","MagicCharacter"})
 	public static String replaceNonValidXMLCharacters(String in, char replacementChar)
 	{
 		StringBuilder out = null;
