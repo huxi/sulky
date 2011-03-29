5f468a78aa0feb1a89a6632277462d5182f56f3d
Joern Huxhorn <huxhorn@users.sourceforge.net>
2011-03-29 15:12:15 +0200

    Copyright notice 2010 => 2011

--- sulky-resources/src/test/java/de/huxhorn/sulky/resources/PathToolsTest.java
+++ sulky-resources/src/test/java/de/huxhorn/sulky/resources/PathToolsTest.java
@@ -1,6 +1,6 @@
 /*
  * sulky-resources - inheritance-safe class resources.
- * Copyright (C) 2002-2010 Joern Huxhorn
+ * Copyright (C) 2002-2011 Joern Huxhorn
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
  */
 
 /*
- * Copyright 2002-2010 Joern Huxhorn
+ * Copyright 2002-2011 Joern Huxhorn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
