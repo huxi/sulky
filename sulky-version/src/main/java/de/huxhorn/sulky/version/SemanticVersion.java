102201158c196fc3fa5fa575da5c799e306672ec
Joern Huxhorn <huxhorn@users.sourceforge.net>
2015-07-16 17:44:39 +0200

    Added SemanticVersion.
    Implementation corresponds to Semantic Versioning 2.0.0
    See http://semver.org/

---
+++ sulky-version/src/main/java/de/huxhorn/sulky/version/SemanticVersion.java
@@ -0,0 +1,423 @@
+/*
+ * sulky-modules - several general-purpose modules.
+ * Copyright (C) 2007-2015 Joern Huxhorn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Copyright 2007-2015 Joern Huxhorn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.huxhorn.sulky.version;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Version implementation according to <a href="http://semver.org/">Semantic Versioning 2.0.0</a>.
+ *
+ *
+ */
+public class SemanticVersion
+	implements Serializable, Comparable<SemanticVersion>
+{
+	private static final long serialVersionUID = -2336778957623151857L;
+
+	private static final String VERSION_NUMBER_PATTERN_STRING = "((0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*))";
+
+	private static final String IDENTIFIER_ELEMENT_PATTERN_STRING = "([-a-zA-Z0-9]+)";
+
+	private static final String IDENTIFIER_PATTERN_STRING = "(" + IDENTIFIER_ELEMENT_PATTERN_STRING + "(\\." + IDENTIFIER_ELEMENT_PATTERN_STRING + ")*)";
+
+
+	private static final String SEMANTIC_VERSION_PATTERN_STRING =
+			VERSION_NUMBER_PATTERN_STRING
+			+ "(-" + IDENTIFIER_PATTERN_STRING + ")?"
+			+ "(\\+" + IDENTIFIER_PATTERN_STRING + ")?";
+
+	private static final Pattern SEMANTIC_VERSION_PATTERN = Pattern.compile(SEMANTIC_VERSION_PATTERN_STRING);
+	private static final Pattern IDENTIFIER_ELEMENT_PATTERN = Pattern.compile(IDENTIFIER_ELEMENT_PATTERN_STRING);
+
+	private static final int MAJOR_GROUP_INDEX = 2;
+	private static final int MINOR_GROUP_INDEX = 3;
+	private static final int PATCH_GROUP_INDEX = 4;
+
+	private static final int PRE_RELEASE_GROUP_INDEX = 6;
+	private static final int BUILD_METADATA_GROUP_INDEX = 11;
+	private static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+	private final long major;
+	private final long minor;
+	private final long patch;
+	private final String[] preRelease;
+	private final String[] buildMetadata;
+	private transient String versionString;
+
+	public static SemanticVersion parse(String versionString)
+	{
+		if(versionString == null)
+		{
+			throw new NullPointerException("versionString must not be null!");
+		}
+
+		Matcher matcher = SEMANTIC_VERSION_PATTERN.matcher(versionString);
+		if(!matcher.matches())
+		{
+			throw new IllegalArgumentException("'"+versionString+"' is not a valid semantic version!");
+		}
+
+		long major = Long.parseLong(matcher.group(MAJOR_GROUP_INDEX));
+		long minor = Long.parseLong(matcher.group(MINOR_GROUP_INDEX));
+		long patch = Long.parseLong(matcher.group(PATCH_GROUP_INDEX));
+
+		String preReleaseGroup = matcher.group(PRE_RELEASE_GROUP_INDEX);
+		String[] preRelease = EMPTY_STRING_ARRAY;
+		if(preReleaseGroup != null)
+		{
+			preRelease = preReleaseGroup.split("\\.");
+		}
+
+		String buildMetadataGroup = matcher.group(BUILD_METADATA_GROUP_INDEX);
+		String[] buildMetadata = EMPTY_STRING_ARRAY;
+		if(buildMetadataGroup != null)
+		{
+			buildMetadata = buildMetadataGroup.split("\\.");
+		}
+
+		return new SemanticVersion(major, minor, patch, preRelease, buildMetadata);
+	}
+
+	public SemanticVersion(long major, long minor, long patch)
+	{
+		this(major, minor, patch, EMPTY_STRING_ARRAY, EMPTY_STRING_ARRAY);
+	}
+
+	public SemanticVersion(long major, long minor, long patch, String[] preRelease)
+	{
+		this(major, minor, patch, preRelease, EMPTY_STRING_ARRAY);
+	}
+
+	public SemanticVersion(long major, long minor, long patch, String[] preRelease, String[] buildMetadata)
+	{
+		if(major < 0)
+		{
+			throw new IllegalArgumentException("major must not be negative!");
+		}
+		if(minor < 0)
+		{
+			throw new IllegalArgumentException("minor must not be negative!");
+		}
+		if(patch < 0)
+		{
+			throw new IllegalArgumentException("patch must not be negative!");
+		}
+		if(preRelease == null)
+		{
+			preRelease = EMPTY_STRING_ARRAY;
+		}
+		if(buildMetadata == null)
+		{
+			buildMetadata = EMPTY_STRING_ARRAY;
+		}
+
+		for (String current : preRelease)
+		{
+			if(current == null)
+			{
+				throw new IllegalArgumentException("preRelease must not contain null!");
+			}
+			if(!IDENTIFIER_ELEMENT_PATTERN.matcher(current).matches())
+			{
+				throw new IllegalArgumentException("preRelease identifier '"+current+"' is invalid!");
+			}
+		}
+
+		for (String current : buildMetadata)
+		{
+			if(current == null)
+			{
+				throw new IllegalArgumentException("buildMetadata must not contain null!");
+			}
+			if(!IDENTIFIER_ELEMENT_PATTERN.matcher(current).matches())
+			{
+				throw new IllegalArgumentException("buildMetadata identifier '"+current+"' is invalid!");
+			}
+		}
+
+		// ensure immutability
+		if(preRelease.length != 0)
+		{
+			String[] newArray = new String[preRelease.length];
+			System.arraycopy(preRelease, 0, newArray, 0, preRelease.length);
+			preRelease = newArray;
+		}
+
+		if(buildMetadata.length != 0)
+		{
+			String[] newArray = new String[buildMetadata.length];
+			System.arraycopy(buildMetadata, 0, newArray, 0, buildMetadata.length);
+			buildMetadata = newArray;
+		}
+
+		this.major = major;
+		this.minor = minor;
+		this.patch = patch;
+		this.preRelease = preRelease;
+		this.buildMetadata = buildMetadata;
+	}
+
+	public long getMajor()
+	{
+		return major;
+	}
+
+	public long getMinor()
+	{
+		return minor;
+	}
+
+	public long getPatch()
+	{
+		return patch;
+	}
+
+	public List<String> getPreRelease()
+	{
+		return Arrays.asList(preRelease);
+	}
+
+	public List<String> getBuildMetadata()
+	{
+		return Arrays.asList(buildMetadata);
+	}
+
+	private String generateString()
+	{
+		StringBuilder result = new StringBuilder();
+		result.append(major).append('.').append(minor).append('.').append(patch);
+		if(preRelease.length > 0)
+		{
+			result.append('-');
+			boolean first = true;
+			for (String current : preRelease)
+			{
+				if(first)
+				{
+					first = false;
+				}
+				else
+				{
+					result.append('.');
+				}
+				result.append(current);
+			}
+		}
+		if(buildMetadata.length > 0)
+		{
+			result.append('+');
+			boolean first = true;
+			for (String current : buildMetadata)
+			{
+				if(first)
+				{
+					first = false;
+				}
+				else
+				{
+					result.append('.');
+				}
+				result.append(current);
+			}
+		}
+		return result.toString();
+	}
+
+	@Override
+	public String toString()
+	{
+		if(versionString == null)
+		{
+			versionString = generateString();
+		}
+		return versionString;
+	}
+
+	@Override
+	public int compareTo(SemanticVersion other)
+	{
+		if(other == null)
+		{
+			throw new NullPointerException("other must not be null!");
+		}
+		if(major < other.major)
+		{
+			return -1;
+		}
+		if(major > other.major)
+		{
+			return 1;
+		}
+
+		if(minor < other.minor)
+		{
+			return -1;
+		}
+		if(minor > other.minor)
+		{
+			return 1;
+		}
+
+		if(patch < other.patch)
+		{
+			return -1;
+		}
+		if(patch > other.patch)
+		{
+			return 1;
+		}
+		if(preRelease.length == 0)
+		{
+			if(other.preRelease.length == 0)
+			{
+				return 0;
+			}
+			return 1;
+		}
+		if(other.preRelease.length == 0)
+		{
+			return -1;
+		}
+
+		int maxLength = Integer.max(preRelease.length, other.preRelease.length);
+
+		for(int i=0 ; i<maxLength ; i++)
+		{
+			String a;
+			String b;
+			if(i < preRelease.length)
+			{
+				a = preRelease[i];
+			}
+			else
+			{
+				return -1;
+			}
+			if(i < other.preRelease.length)
+			{
+				b = other.preRelease[i];
+			}
+			else
+			{
+				return 1;
+			}
+
+			boolean aIsNumber = true;
+			boolean bIsNumber = true;
+
+			long aAsNumber = 0;
+			long bAsNumber = 0;
+
+			try
+			{
+				aAsNumber = Long.parseLong(a);
+			}
+			catch(NumberFormatException ex)
+			{
+				aIsNumber = false;
+			}
+			try
+			{
+				bAsNumber = Long.parseLong(b);
+			}
+			catch(NumberFormatException ex)
+			{
+				bIsNumber = false;
+			}
+			if(aIsNumber)
+			{
+				if(bIsNumber)
+				{
+					if(aAsNumber == bAsNumber)
+					{
+						continue;
+					}
+					if(aAsNumber < bAsNumber)
+					{
+						return -1;
+					}
+					return 1;
+				}
+				return -1;
+			}
+			if(bIsNumber)
+			{
+				return 1;
+			}
+			// neither a nor b are numbers
+			int compared = a.compareTo(b);
+			if(compared == 0)
+			{
+				continue;
+			}
+			if(compared < 0)
+			{
+				return -1;
+			}
+			return 1;
+		}
+
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		SemanticVersion that = (SemanticVersion) o;
+
+		if (major != that.major) return false;
+		if (minor != that.minor) return false;
+		if (patch != that.patch) return false;
+		return Arrays.equals(preRelease, that.preRelease) && Arrays.equals(buildMetadata, that.buildMetadata);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int result = (int) (major ^ (major >>> 32));
+		result = 31 * result + (int) (minor ^ (minor >>> 32));
+		result = 31 * result + (int) (patch ^ (patch >>> 32));
+		result = 31 * result + Arrays.hashCode(preRelease);
+		result = 31 * result + Arrays.hashCode(buildMetadata);
+		return result;
+	}
+}
---
+++ sulky-version/src/test/groovy/de/huxhorn/sulky/version/SemanticVersionSpec.groovy
@@ -0,0 +1,417 @@
+/*
+ * sulky-modules - several general-purpose modules.
+ * Copyright (C) 2007-2015 Joern Huxhorn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Copyright 2007-2015 Joern Huxhorn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.huxhorn.sulky.version
+
+import de.huxhorn.sulky.junit.JUnitTools
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SemanticVersionSpec
+    extends Specification
+{
+
+    @Unroll("'#versionString' is a valid semantic version.")
+    def validVersions(String versionString, SemanticVersion expectedValue) {
+        when:
+        SemanticVersion version = SemanticVersion.parse(versionString)
+        String generatedVersionString = version.toString()
+        String regeneratedVersionString = version.toString() // check transient variable
+
+        then:
+        // using explicit equals() since == uses compareTo if Comparable is implemented.
+        version.equals(expectedValue)
+        version.hashCode() == expectedValue.hashCode()
+        generatedVersionString == versionString
+        generatedVersionString == expectedValue.toString()
+        generatedVersionString == regeneratedVersionString
+
+        where:
+        versionString                | expectedValue
+        '0.1.2'                      | new SemanticVersion(0, 1, 2)
+        '1.0.0'                      | new SemanticVersion(1, 0, 0)
+        '1.0.0'                      | new SemanticVersion(1, 0, 0, null, null)
+        '1.0.0+20130313144700'       | new SemanticVersion(1, 0, 0, null, ['20130313144700'] as String[])
+        '1.0.0-0.3.7'                | new SemanticVersion(1, 0, 0, ['0', '3', '7'] as String[])
+        '1.0.0-alpha'                | new SemanticVersion(1, 0, 0, ['alpha'] as String[])
+        '1.0.0-alpha+001'            | new SemanticVersion(1, 0, 0, ['alpha'] as String[], ['001'] as String[])
+        '1.0.0-alpha.1'              | new SemanticVersion(1, 0, 0, ['alpha', '1'] as String[])
+        '1.0.0-alpha.beta'           | new SemanticVersion(1, 0, 0, ['alpha', 'beta'] as String[])
+        '1.0.0-beta'                 | new SemanticVersion(1, 0, 0, ['beta'] as String[])
+        '1.0.0-beta+exp.sha.5114f85' | new SemanticVersion(1, 0, 0, ['beta'] as String[], ['exp', 'sha', '5114f85'] as String[])
+        '1.0.0-beta.2'               | new SemanticVersion(1, 0, 0, ['beta', '2'] as String[])
+        '1.0.0-beta.11'              | new SemanticVersion(1, 0, 0, ['beta', '11'] as String[])
+        '1.0.0-rc.1'                 | new SemanticVersion(1, 0, 0, ['rc', '1'] as String[])
+        '1.0.0-x.7.z.92'             | new SemanticVersion(1, 0, 0, ['x', '7', 'z', '92'] as String[])
+        '1.9.0'                      | new SemanticVersion(1, 9, 0)
+        '1.10.0'                     | new SemanticVersion(1, 10, 0)
+        '1.11.0'                     | new SemanticVersion(1, 11, 0)
+        '2.0.0'                      | new SemanticVersion(2, 0, 0)
+        '2.1.0'                      | new SemanticVersion(2, 1, 0)
+        '2.1.1'                      | new SemanticVersion(2, 1, 1)
+
+    }
+
+    @Unroll('#versionAString #compareString #versionBString.')
+    def 'comparing instances works using compareTo'() {
+        when:
+        SemanticVersion versionA = SemanticVersion.parse(versionAString)
+        SemanticVersion versionB = SemanticVersion.parse(versionBString)
+        int compareToAB = versionA.compareTo(versionB)
+        int compareToBA = versionB.compareTo(versionA)
+
+        then:
+        compareToAB == expectedResult
+        compareToAB ==  -1 * compareToBA
+
+
+        // 1.9.0 -> 1.10.0 -> 1.11.0
+        // 0.x.y
+        // 1.0.0-alpha, 1.0.0-alpha.1, 1.0.0-0.3.7, 1.0.0-x.7.z.92
+        // 1.0.0-alpha+001, 1.0.0+20130313144700, 1.0.0-beta+exp.sha.5114f85
+        // 1.0.0 < 2.0.0 < 2.1.0 < 2.1.1
+        // 1.0.0-alpha < 1.0.0
+        // 1.0.0-alpha < 1.0.0-alpha.1 < 1.0.0-alpha.beta < 1.0.0-beta < 1.0.0-beta.2 < 1.0.0-beta.11 < 1.0.0-rc.1 < 1.0.0
+
+        where:
+        versionAString     | versionBString     | expectedResult
+        '0.9.0'            | '1.0.0'            | -1
+        '1.9.0'            | '1.10.0'           | -1
+        '1.9.0'            | '1.11.0'           | -1
+        '1.10.0'           | '1.11.0'           | -1
+        '1.11.0'           | '1.11.1'           | -1
+        '1.0.0'            | '2.0.0'            | -1
+        '1.0.0'            | '2.1.0'            | -1
+        '1.0.0'            | '2.1.1'            | -1
+        '2.0.0'            | '2.1.0'            | -1
+        '2.0.0'            | '2.1.1'            | -1
+        '2.1.0'            | '2.1.1'            | -1
+        '1.0.0-alpha'      | '1.0.0'            | -1
+        '1.0.0-alpha'      | '1.0.0-alpha.1'    | -1
+        '1.0.0-alpha'      | '1.0.0-alpha.beta' | -1
+        '1.0.0-alpha'      | '1.0.0-beta'       | -1
+        '1.0.0-alpha'      | '1.0.0-beta.2'     | -1
+        '1.0.0-alpha'      | '1.0.0-beta.11'    | -1
+        '1.0.0-alpha'      | '1.0.0-rc.1'       | -1
+        '1.0.0-alpha.1'    | '1.0.0-alpha.beta' | -1
+        '1.0.0-alpha.1'    | '1.0.0-beta'       | -1
+        '1.0.0-alpha.1'    | '1.0.0-beta.2'     | -1
+        '1.0.0-alpha.1'    | '1.0.0-beta.11'    | -1
+        '1.0.0-alpha.1'    | '1.0.0-rc.1'       | -1
+        '1.0.0-alpha.beta' | '1.0.0-beta'       | -1
+        '1.0.0-alpha.beta' | '1.0.0-beta.2'     | -1
+        '1.0.0-alpha.beta' | '1.0.0-beta.11'    | -1
+        '1.0.0-alpha.beta' | '1.0.0-rc.1'       | -1
+        '1.0.0-beta'       | '1.0.0-beta.2'     | -1
+        '1.0.0-beta'       | '1.0.0-beta.11'    | -1
+        '1.0.0-beta'       | '1.0.0-rc.1'       | -1
+        '1.0.0-beta.2'     | '1.0.0-beta.11'    | -1
+        '1.0.0-beta.2'     | '1.0.0-rc.1'       | -1
+        '1.0.0-beta.11'    | '1.0.0-rc.1'       | -1
+        '1.9.0'            | '1.9.0'            | 0
+        '1.0.0-alpha'      | '1.0.0-alpha+001'  | 0
+        '1.0.0-alpha.1'    | '1.0.0-alpha.1'    | 0
+
+        compareString = compareString(expectedResult)
+    }
+
+    @Unroll('#versionAString #compareString #versionBString.')
+    def 'comparing instances works using equals and hashCode'() {
+        when:
+        SemanticVersion versionA = SemanticVersion.parse(versionAString)
+        SemanticVersion versionB = SemanticVersion.parse(versionBString)
+
+        then:
+        if(expectedResult) {
+            assert versionA.equals(versionB)
+            assert versionA.hashCode() == versionB.hashCode()
+        }
+        else {
+            assert !versionA.equals(versionB)
+        }
+
+        where:
+        versionAString     | versionBString      | expectedResult
+        '0.9.0'            | '1.0.0'             | false
+        '1.9.0'            | '1.10.0'            | false
+        '1.9.0'            | '1.11.0'            | false
+        '1.10.0'           | '1.11.0'            | false
+        '1.11.0'           | '1.11.1'            | false
+        '1.0.0'            | '2.0.0'             | false
+        '1.0.0'            | '2.1.0'             | false
+        '1.0.0'            | '2.1.1'             | false
+        '2.0.0'            | '2.1.0'             | false
+        '2.0.0'            | '2.1.1'             | false
+        '2.1.0'            | '2.1.1'             | false
+        '1.0.0-alpha'      | '1.0.0'             | false
+        '1.0.0-alpha'      | '1.0.0-alpha.1'     | false
+        '1.0.0-alpha'      | '1.0.0-alpha.beta'  | false
+        '1.0.0-alpha'      | '1.0.0-beta'        | false
+        '1.0.0-alpha'      | '1.0.0-beta.2'      | false
+        '1.0.0-alpha'      | '1.0.0-beta.11'     | false
+        '1.0.0-alpha'      | '1.0.0-rc.1'        | false
+        '1.0.0-alpha.1'    | '1.0.0-alpha.beta'  | false
+        '1.0.0-alpha.1'    | '1.0.0-beta'        | false
+        '1.0.0-alpha.1'    | '1.0.0-beta.2'      | false
+        '1.0.0-alpha.1'    | '1.0.0-beta.11'     | false
+        '1.0.0-alpha.1'    | '1.0.0-rc.1'        | false
+        '1.0.0-alpha.beta' | '1.0.0-beta'        | false
+        '1.0.0-alpha.beta' | '1.0.0-beta.2'      | false
+        '1.0.0-alpha.beta' | '1.0.0-beta.11'     | false
+        '1.0.0-alpha.beta' | '1.0.0-rc.1'        | false
+        '1.0.0-beta'       | '1.0.0-beta.2'      | false
+        '1.0.0-beta'       | '1.0.0-beta.11'     | false
+        '1.0.0-beta'       | '1.0.0-rc.1'        | false
+        '1.0.0-beta.2'     | '1.0.0-beta.11'     | false
+        '1.0.0-beta.2'     | '1.0.0-rc.1'        | false
+        '1.0.0-beta.11'    | '1.0.0-rc.1'        | false
+        '1.9.0'            | '1.9.0'             | true
+        '1.0.0-alpha'      | '1.0.0-alpha+001'   | false
+        '1.0.0-alpha.1'    | '1.0.0-alpha.1'     | true
+        '1.0.0-alpha+001'  | '2.0.0-alpha+001'   | false
+        '1.0.0-alpha+001'  | '1.1.0-alpha+001'   | false
+        '1.0.0-alpha+001'  | '1.0.1-alpha+001'   | false
+        '1.0.0-alpha+001'  | '1.0.1-alpha.1+001' | false
+        '1.0.0-alpha+001'  | '1.0.1-alpha1+001'  | false
+        '1.0.0-alpha+001'  | '1.0.1-alpha+001.1' | false
+        '1.0.0-alpha+001'  | '1.0.1-alpha+002'   | false
+
+        compareString = expectedResult? 'equals' : 'does not equal'
+    }
+
+    def "null preRelease value"() {
+        when:
+        new SemanticVersion(1,0,0, ['foo', null, 'bar'] as String[])
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == 'preRelease must not contain null!'
+    }
+
+    @Unroll("'#illegalValue' is an illegal preRelease identifier.")
+    def "illegal preRelease value"() {
+        when:
+        new SemanticVersion(1,0,0, ['foo', illegalValue, 'bar'] as String[])
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == 'preRelease identifier \''+illegalValue+'\' is invalid!'
+
+        where:
+        illegalValue << [
+                '',
+                '$foo'
+        ]
+    }
+
+    def "null buildMetadata value"() {
+        when:
+        new SemanticVersion(1,0,0, null, ['foo', null, 'bar'] as String[])
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == 'buildMetadata must not contain null!'
+    }
+
+    @Unroll("'#illegalValue' is an illegal preRelease identifier.")
+    def "illegal buildMetadata value"() {
+        when:
+        new SemanticVersion(1,0,0, null, ['foo', illegalValue, 'bar'] as String[])
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == 'buildMetadata identifier \''+illegalValue+'\' is invalid!'
+
+        where:
+        illegalValue << [
+                '',
+                '$foo'
+        ]
+    }
+
+    def "ensure getPreRelease() immutability"() {
+        when:
+        new SemanticVersion(1,0,0, ['foo', 'bar'] as String[]).preRelease.remove(0)
+
+        then:
+        thrown(UnsupportedOperationException)
+    }
+
+    def "ensure getBuildMetadata() immutability"() {
+        when:
+        new SemanticVersion(1,0,0, null, ['foo', 'bar'] as String[]).buildMetadata.remove(0)
+
+        then:
+        thrown(UnsupportedOperationException)
+    }
+
+    def "ensure preRelease array immutability"() {
+        when:
+        String[] identifiers = ['foo', 'bar']
+        SemanticVersion version = new SemanticVersion(1,0,0, identifiers)
+        identifiers[0] = 'fooBar'
+
+        then:
+        version.preRelease[0] == 'foo'
+    }
+
+    def "ensure buildMetadata array immutability"() {
+        when:
+        String[] identifiers = ['foo', 'bar']
+        SemanticVersion version = new SemanticVersion(1,0,0, null, identifiers)
+        identifiers[0] = 'fooBar'
+
+        then:
+        version.buildMetadata[0] == 'foo'
+    }
+    def "equals(null)"() {
+        expect:
+        !new SemanticVersion(1,0,0).equals(null)
+    }
+
+    def "equals(Object)"() {
+        expect:
+        !new SemanticVersion(1,0,0).equals(new Object())
+    }
+
+    def "equals(this)"() {
+        when:
+        SemanticVersion version = new SemanticVersion(1,0,0)
+
+        then:
+        version.equals(version)
+    }
+
+    def "parse(null) throws exception."() {
+        when:
+        SemanticVersion.parse(null)
+
+        then:
+        NullPointerException ex = thrown()
+        ex.message == 'versionString must not be null!'
+    }
+
+    def "compareTo(null) throws exception."() {
+        when:
+        new SemanticVersion(1,0,0).compareTo(null)
+
+        then:
+        NullPointerException ex = thrown()
+        ex.message == 'other must not be null!'
+    }
+
+    @Unroll("parse('#versionString') throws exception.")
+    def "parsing invalid strings throws proper exception."() {
+        when:
+        SemanticVersion.parse(versionString)
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == "'$versionString' is not a valid semantic version!".toString()
+
+        where:
+        versionString << [
+                '1.0'
+        ]
+    }
+
+    def "invalid major number"() {
+        when:
+        new SemanticVersion(-1,0,0)
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == 'major must not be negative!'
+    }
+
+    def "invalid minor number"() {
+        when:
+        new SemanticVersion(0,-1,0)
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == 'minor must not be negative!'
+    }
+
+    def "invalid patch number"() {
+        when:
+        new SemanticVersion(0,0,-1)
+
+        then:
+        IllegalArgumentException ex = thrown()
+        ex.message == 'patch must not be negative!'
+    }
+
+    def "check getters"() {
+        when:
+        SemanticVersion version = new SemanticVersion(major, minor, patch, preRelease, buildMetadata)
+
+        then:
+        version.major == major
+        version.minor == minor
+        version.patch == patch
+        version.preRelease == preRelease as List<String>
+        version.buildMetadata == buildMetadata as List<String>
+
+        where:
+        major | minor | patch | preRelease            | buildMetadata
+        1     | 2     | 3     | [] as String[]        | [] as String[]
+        1     | 2     | 3     | ['alpha'] as String[] | ['foo'] as String[]
+    }
+
+    private static String compareString(int value)
+    {
+        if(value < 0) {
+            return 'is less than'
+        }
+
+        if(value > 0) {
+            return 'is greater than'
+        }
+
+        return 'is equal to'
+    }
+
+    def 'serializable'() {
+        expect:
+        JUnitTools.testSerialization(object)
+
+        where:
+        object << [
+                new SemanticVersion(1, 2, 3),
+                new SemanticVersion(1, 2, 3, ['foo', 'bar'] as String[]),
+                new SemanticVersion(1, 2, 3, ['foo', 'bar'] as String[], ['17', 'foobar', '1'] as String[])
+        ]
+    }
+}
