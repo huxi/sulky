9c73b91eb4323a9caf3e551ebc97f0d2e3707974
Joern Huxhorn <huxhorn@users.sourceforge.net>
2015-11-11 10:59:47 +0100

    Thread.interrupted() instead of sleep(1).

--- sulky-tasks/src/main/java/de/huxhorn/sulky/tasks/AbstractProgressingCallable.java
+++ sulky-tasks/src/main/java/de/huxhorn/sulky/tasks/AbstractProgressingCallable.java
@@ -1,6 +1,6 @@
 /*
  * sulky-modules - several general-purpose modules.
- * Copyright (C) 2007-2011 Joern Huxhorn
+ * Copyright (C) 2007-2015 Joern Huxhorn
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
  */
 
 /*
- * Copyright 2007-2011 Joern Huxhorn
+ * Copyright 2007-2015 Joern Huxhorn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -136,13 +136,19 @@ public abstract class AbstractProgressingCallable<T>
 					if(lastSleepStep + laterSleepSteps < currentStep)
 					{
 						lastSleepStep = currentStep;
-						Thread.sleep(1);
+						if(Thread.interrupted())
+						{
+							throw new InterruptedException();
+						}
 					}
 				}
 				else if(lastSleepStep + initialSleepSteps < currentStep)
 				{
 					lastSleepStep = currentStep;
-					Thread.sleep(1);
+					if(Thread.interrupted())
+					{
+						throw new InterruptedException();
+					}
 				}
 			}
 		}
