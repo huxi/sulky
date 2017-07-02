f7c5a0ab766e1f66d1d73a9b85b90a3eef6b5bf6
Joern Huxhorn <huxhorn@users.sourceforge.net>
2017-07-02 13:38:19 +0200

    Integer.max was added in Java 1.8

--- sulky-version/src/main/java/de/huxhorn/sulky/version/Jep223JavaVersion.java
+++ sulky-version/src/main/java/de/huxhorn/sulky/version/Jep223JavaVersion.java
@@ -1,6 +1,6 @@
 /*
  * sulky-modules - several general-purpose modules.
- * Copyright (C) 2007-2015 Joern Huxhorn
+ * Copyright (C) 2007-2017 Joern Huxhorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
  */
 
 /*
- * Copyright 2007-2015 Joern Huxhorn
+ * Copyright 2007-2017 Joern Huxhorn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -502,7 +502,11 @@ public class Jep223JavaVersion
 		}
 		Integer[] thisVersionNumbers = versionNumbers;
 		Integer[] otherVersionNumbers = other.versionNumbers;
-		int maxLength = Integer.max(thisVersionNumbers.length, otherVersionNumbers.length);
+		int maxLength = thisVersionNumbers.length;
+		if(maxLength < otherVersionNumbers.length)
+		{
+			maxLength = otherVersionNumbers.length;
+		}
 		for(int i=0;i<maxLength;i++)
 		{
 			int thisCurrent = 0;
