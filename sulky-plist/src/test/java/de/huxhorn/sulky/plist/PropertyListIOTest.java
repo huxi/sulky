6e461e6cc9dca28562a3718feeda256e7914f3f9
Joern Huxhorn <huxhorn@users.sourceforge.net>
2018-12-10 11:39:24 +0100

    PMD 6.10.0

--- sulky-plist/src/test/java/de/huxhorn/sulky/plist/PropertyListIOTest.java
+++ sulky-plist/src/test/java/de/huxhorn/sulky/plist/PropertyListIOTest.java
@@ -1,6 +1,6 @@
 /*
  * sulky-modules - several general-purpose modules.
- * Copyright (C) 2007-2016 Joern Huxhorn
+ * Copyright (C) 2007-2018 Joern Huxhorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
  */
 
 /*
- * Copyright 2007-2016 Joern Huxhorn
+ * Copyright 2007-2018 Joern Huxhorn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -296,7 +296,7 @@ public class PropertyListIOTest
 		throws Throwable
 	{
 		PropertyList list=new PropertyList();
-		Date value=new Date(1234567890000L);
+		Date value=new Date(1_234_567_890_000L);
 		list.setRoot(value);
 		check(list, true);
 	}
