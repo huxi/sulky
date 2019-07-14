7f362d14b98fe035f5fd29a1fe2c58a62d94d58b
Joern Huxhorn <huxhorn@users.sourceforge.net>
2019-07-14 12:51:42 +0200

    protobuf 3.9.0

--- sulky-codec-filebuffer/src/main/java/de/huxhorn/sulky/codec/filebuffer/generated/MetaDataProto.java
+++ sulky-codec-filebuffer/src/main/java/de/huxhorn/sulky/codec/filebuffer/generated/MetaDataProto.java
@@ -68,6 +68,13 @@ public final class MetaDataProto {
     }
 
     @java.lang.Override
+    @SuppressWarnings({"unused"})
+    protected java.lang.Object newInstance(
+        UnusedPrivateParameter unused) {
+      return new MetaData();
+    }
+
+    @java.lang.Override
     public final com.google.protobuf.UnknownFieldSet
     getUnknownFields() {
       return this.unknownFields;
@@ -946,6 +953,13 @@ public final class MetaDataProto {
     }
 
     @java.lang.Override
+    @SuppressWarnings({"unused"})
+    protected java.lang.Object newInstance(
+        UnusedPrivateParameter unused) {
+      return new MapEntry();
+    }
+
+    @java.lang.Override
     public final com.google.protobuf.UnknownFieldSet
     getUnknownFields() {
       return this.unknownFields;
@@ -1677,18 +1691,10 @@ public final class MetaDataProto {
       "lue\030\002 \001(\tB/\n+de.huxhorn.sulky.codec.file" +
       "buffer.generatedH\001"
     };
-    com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
-        new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
-          public com.google.protobuf.ExtensionRegistry assignDescriptors(
-              com.google.protobuf.Descriptors.FileDescriptor root) {
-            descriptor = root;
-            return null;
-          }
-        };
-    com.google.protobuf.Descriptors.FileDescriptor
+    descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
         new com.google.protobuf.Descriptors.FileDescriptor[] {
-        }, assigner);
+        });
     internal_static_de_huxhorn_sulky_codec_filebuffer_MetaData_descriptor =
       getDescriptor().getMessageTypes().get(0);
     internal_static_de_huxhorn_sulky_codec_filebuffer_MetaData_fieldAccessorTable = new
