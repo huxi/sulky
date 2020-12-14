49a7974667ae5cf06eb8ba1649822c3a93f2f495
Joern Huxhorn <jhuxhorn@googlemail.com>
2020-12-14 16:54:44 +0100

    pmd 6.30.0

--- sulky-tasks/src/main/java/de/huxhorn/sulky/tasks/TaskManager.java
+++ sulky-tasks/src/main/java/de/huxhorn/sulky/tasks/TaskManager.java
@@ -1,6 +1,6 @@
 /*
  * sulky-modules - several general-purpose modules.
- * Copyright (C) 2007-2018 Joern Huxhorn
+ * Copyright (C) 2007-2020 Joern Huxhorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
  */
 
 /*
- * Copyright 2007-2018 Joern Huxhorn
+ * Copyright 2007-2020 Joern Huxhorn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -807,6 +807,7 @@ public class TaskManager<T>
 		}
 
 		@Override
+		@SuppressWarnings("PMD.CompareObjectsWithEquals")
 		public boolean equals(Object o)
 		{
 			if (this == o) return true;
